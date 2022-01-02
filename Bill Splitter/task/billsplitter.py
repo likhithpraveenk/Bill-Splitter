175535b9b204328d9df32aea5211ce35f6ba62c2
Likhith Praveen <lpkatlagunta@gmail.com>
2022-01-02 16:36:55 +0530

    stage 1 completed

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -1,1 +1,10 @@
-# write your code here
+n = int(input("Enter the number of friends joining (including you):\n"))
+participants = {}
+if n <= 0:
+    print("No one is joining for the party")
+else:
+    print("Enter the name of every friend (including you), each on a new line:")
+    for _ in range(n):
+        name = input()
+        participants[name] = 0
+    print(participants)
---
+++ Topics/Assert statement/Checking the bounds/main.py
@@ -0,0 +1,4 @@
+def bounds(x):
+    mini, maxi = 50, 70
+    assert mini <= x <= maxi, "Your number is wrong!"
+    return x
