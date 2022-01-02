2602f1a33b82d93e6d4b4b75a06ed6ddb160ba48
Likhith Praveen <lpkatlagunta@gmail.com>
2022-01-02 17:11:05 +0530

    stage 3 completed

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -1,3 +1,5 @@
+import random
+
 n = int(input("Enter the number of friends joining (including you):\n"))
 participants = {}
 if n <= 0:
@@ -5,11 +7,22 @@ if n <= 0:
 else:
     print("Enter the name of every friend (including you), each on a new line:")
     for _ in range(n):
-        name = input()
-        participants[name] = 0
+        participants[input()] = 0
     # print(participants)
     bill = int(input("Enter the total bill value:\n"))
     rounded = round(bill / n, 2)
-    for key in participants:
-        participants[key] += rounded
-    print(participants)
+    for person in participants:
+        participants[person] += rounded
+    # print(participants)
+    feature_request = input('Do you want to use the "Who is lucky?" feature? Write Yes/No:\n')
+    if feature_request == "Yes":
+        lucky = random.choice(list(participants))
+        print(f"{lucky} is the lucky one!")
+        share_amount = participants[lucky]/n-1
+        for person in participants:
+            if person == lucky:
+                participants[person] = 0
+            else:
+                participants[person] += share_amount
+    else:
+        print("No one is going to be lucky")
